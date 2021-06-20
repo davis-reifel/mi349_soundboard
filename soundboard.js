9bda3e6cf1ce8fcb5cb47007894433946ee6bba2
Your Name <Davis Reifel>
2021-06-19 22:01:18 -0400

    sound

--- soundboard.js
+++ soundboard.js
@@ -2,8 +2,22 @@ var buttonOne = document.getElementById('s1')
 var buttonTwo = document.getElementById('s2')
 var buttonThree = document.getElementById('s3')
 
-buttonOne.addEventListener('click', function () {
+var audioOne = new Audio('sound1.wav')
+var audioTwo = new Audio('sound2.wav')
+var audioThree = new Audio('sound3.wav')
+
+buttonOne.addEventListener('mouseenter', function () {
+    audioOne.play()
+})
+buttonTwo.addEventListener('mouseenter', function () {
     console.log('I was clicked!')
+})
+buttonThree.addEventListener('mouseenter', function () {
+    console.log('I was clicked!')
+})
+
+buttonOne.addEventListener('click', function () {
+    audioOne.play()
 })
 buttonTwo.addEventListener('click', function () {
     console.log('I was clicked!')
