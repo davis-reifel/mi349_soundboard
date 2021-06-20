00abaac0fdb67f57e1345becf81dc4c0be9c9cd5
Your Name <Davis Reifel>
2021-06-19 21:36:39 -0400

    pages

---
+++ soundboard.js
@@ -0,0 +1,13 @@
+var buttonOne = document.getElementById('s1')
+var buttonTwo = document.getElementById('s2')
+var buttonThree = document.getElementById('s3')
+
+buttonOne.addEventListener('click', function () {
+    console.log('I was clicked!')
+})
+buttonTwo.addEventListener('click', function () {
+    console.log('I was clicked!')
+})
+buttonThree.addEventListener('click', function () {
+    console.log('I was clicked!')
+})
